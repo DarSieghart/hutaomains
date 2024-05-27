9fb05b80cc6c152a81db7097403a3fe693117ba8
Morron200IQ <braexejuare@gmail.com>
2024-05-27 16:12:57 -0300

    Functionalities

--- index.php
+++ index.php
@@ -74,49 +74,20 @@ session_start();
         <h1 class="text-white text-center text-4xl font-semibold">Bienvenido a HuTaoMains!</h1>
     </div>
         
-    
-<div id="default-carousel" class="relative w-full " data-carousel="slide">
-   
-    <div class="relative h overflow-hidden rounded-lg md:h-96">
-      
-        <div class=" duration-700 ease-in-out" data-carousel-item>
-            <img src="assets/img/Stars-Shining-Depths-official-wallpaper-genshin.jpg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-        </div>
-
-        <div class="hidden duration-700 ease-in-out" data-carousel-item>
-            <img src="assets/img/wallpaper_for_wanderer_.jpeg" class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-        </div>
-    
-    
-    </div>
-    
-    <div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="0"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2" data-carousel-slide-to="1"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3" data-carousel-slide-to="2"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4" data-carousel-slide-to="3"></button>
-        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5" data-carousel-slide-to="4"></button>
-    </div>
-   
-    <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
-            </svg>
-            <span class="sr-only">Previous</span>
-        </span>
-    </button>
-    <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
-        <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-            <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
-            </svg>
-            <span class="sr-only">Nuevo</span>
-        </span>
-    </button>
+<div class="carousel-container">
+  <div class="carousel-item">
+    <img src="assets/img/WallpaperHome.jpg" alt="Imagen 1">
+  </div>
+  <div class="carousel-item">
+    <img src="assets/img/WallpaperWanderer.jpeg" alt="Imagen 2">
+  </div>
+  <div class="carousel-item">
+    <img src="imagen3.jpg" alt="Imagen 3">
+  </div>
 </div>
+<button id="prevBtn">Anterior</button>
+<button id="nextBtn">Siguiente</button>
 
-    
     <?php } ?>
 
     <?php if (isset ($_SESSION['user'])) {?>
